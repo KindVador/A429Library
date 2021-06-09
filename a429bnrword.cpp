f5fa6f1d7234676331627a202270371d7d680dd8
Florian Contival <11033778+KindVador@users.noreply.github.com>
2021-06-09 22:43:04 +0200

    BnrWord: add getters/setters definitions and tests

--- a429bnrword.cpp
+++ a429bnrword.cpp
@@ -1,11 +1,11 @@
 #include "a429bnrword.hpp"
 
-A429BnrWord::A429BnrWord()
+A429BnrWord::A429BnrWord(): A429Word()
 {
 
 }
 
-A429BnrWord::A429BnrWord(bool isSigned, ushort signBitPos, ushort msbBitPos, ushort lsbBitPos, double resolution) : m_isSigned(isSigned), 
+A429BnrWord::A429BnrWord(bool isSigned, ushort signBitPos, ushort msbBitPos, ushort lsbBitPos, double resolution) : A429Word(), m_isSigned(isSigned), 
                 m_signBitPosition(signBitPos), m_msbBitPosition(msbBitPos), m_lsbBitPosition(lsbBitPos), m_resolution(resolution)
 {
 
@@ -26,4 +26,54 @@ A429BnrWord::A429BnrWord(std::string value, bool labelNumberMsbFirst, int base, 
 A429BnrWord::~A429BnrWord()
 {
 
+}
+
+bool A429BnrWord::isSigned() const
+{
+    return m_isSigned;
+}
+
+void A429BnrWord::setIsSigned(const bool value)
+{
+    m_isSigned = value;
+}
+
+ushort A429BnrWord::signBitPosition() const
+{
+    return m_signBitPosition;
+}
+
+void A429BnrWord::setSignBitPosition(const ushort position)
+{
+    m_signBitPosition = position;
+}
+
+ushort A429BnrWord::msbBitPosition() const
+{
+    return m_msbBitPosition;
+}
+
+void A429BnrWord::setMsbBitPosition(const ushort position)
+{
+    m_msbBitPosition = position;
+}
+
+ushort A429BnrWord::lsbBitPosition() const
+{
+    return m_lsbBitPosition;
+}
+
+void A429BnrWord::setLsbBitPosition(const ushort position)
+{
+    m_lsbBitPosition = position;
+}
+
+double A429BnrWord::resolution() const
+{
+    return m_resolution;
+}
+
+void A429BnrWord::setResolution(const double value)
+{
+    m_resolution = value;
 }
--- tests/a429bnrwordtests.cpp
+++ tests/a429bnrwordtests.cpp
@@ -59,3 +59,85 @@ TEST(A429BnrWordTest, StringConstructorTest) {
     EXPECT_DOUBLE_EQ(wd.getBnrValue(false, 29, 28, 11, 1.0),  262143.0);
     EXPECT_EQ(wd.getLabelAsBinaryString(true), "11111111") << "getLabelAsBinaryString(true) returned " << wd.getLabelAsBinaryString(true) << " instead of 11111111";
 }
+
+TEST(A429BnrWordTest, GetterIsSigned) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_TRUE(wd.isSigned());
+    wd = A429BnrWord("FFFFFFFF", true, 16, false, 29, 28, 11, 1.0);
+    EXPECT_FALSE(wd.isSigned());
+}
+
+TEST(A429BnrWordTest, SetterIsSigned) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_TRUE(wd.isSigned());
+    wd.setIsSigned(false);
+    EXPECT_FALSE(wd.isSigned());
+    wd = A429BnrWord("FFFFFFFF", true, 16, false, 29, 28, 11, 1.0);
+    EXPECT_FALSE(wd.isSigned());
+    wd.setIsSigned(true);
+    EXPECT_TRUE(wd.isSigned());
+}
+
+TEST(A429BnrWordTest, GetterSignBitPosition) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_EQ(wd.signBitPosition(), 29);
+    wd = A429BnrWord("FFFFFFFF", true, 16, true, 28, 27, 11, 1.0);
+    EXPECT_EQ(wd.signBitPosition(), 28);
+}
+
+TEST(A429BnrWordTest, SetterSignBitPosition) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_EQ(wd.signBitPosition(), 29);
+    wd.setSignBitPosition(28);
+    EXPECT_EQ(wd.signBitPosition(), 28);
+    wd.setSignBitPosition(29);
+    EXPECT_EQ(wd.signBitPosition(), 29);
+}
+
+TEST(A429BnrWordTest, GetterMsbBitPosition) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_EQ(wd.msbBitPosition(), 28);
+    wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 27, 11, 1.0);
+    EXPECT_EQ(wd.msbBitPosition(), 27);
+}
+
+TEST(A429BnrWordTest, SetterMsbBitPosition) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_EQ(wd.msbBitPosition(), 28);
+    wd.setMsbBitPosition(27);
+    EXPECT_EQ(wd.msbBitPosition(), 27);
+    wd.setMsbBitPosition(28);
+    EXPECT_EQ(wd.msbBitPosition(), 28);
+}
+
+TEST(A429BnrWordTest, GetterLsbBitPosition) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_EQ(wd.lsbBitPosition(), 11);
+    wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 27, 12, 1.0);
+    EXPECT_EQ(wd.lsbBitPosition(), 12);
+}
+
+TEST(A429BnrWordTest, SetterLsbBitPosition) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_EQ(wd.lsbBitPosition(), 11);
+    wd.setLsbBitPosition(12);
+    EXPECT_EQ(wd.lsbBitPosition(), 12);
+    wd.setLsbBitPosition(11);
+    EXPECT_EQ(wd.lsbBitPosition(), 11);
+}
+
+TEST(A429BnrWordTest, GetterResolution) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_EQ(wd.resolution(), 1.0);
+    wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 0.5);
+    EXPECT_EQ(wd.resolution(), 0.5);
+}
+
+TEST(A429BnrWordTest, SetterResolution) {
+    A429BnrWord wd = A429BnrWord("FFFFFFFF", true, 16, true, 29, 28, 11, 1.0);
+    EXPECT_EQ(wd.resolution(), 1.0);
+    wd.setResolution(0.5);
+    EXPECT_EQ(wd.resolution(), 0.5);
+    wd.setResolution(1.0);
+    EXPECT_EQ(wd.resolution(), 1.0);
+}
